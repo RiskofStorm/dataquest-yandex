93021c9b4fe53065e5576fc3f18f99589f572369
RiskofStorm <voidnonenullnan@gmail.com>
2019-04-04 20:13:42 +0300

    added warning in bash script at Flask test example

--- yandex/6cap/NLP_Project/YandexDS_FlaskPrj_RiskOfStorm/create_storm_env.sh
+++ yandex/6cap/NLP_Project/YandexDS_FlaskPrj_RiskOfStorm/create_storm_env.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-conda create --name riskofstorm python=3.7 scikit-learn="0.20.1" nltk="3.4" flask="1.0.2" -y
-conda activate riskofstorm
+# Please run it in linux-default terminal, not in Pycharm
+
+
+conda create --name storm python=3.7 scikit-learn="0.20.1" nltk="3.4" flask="1.0.2" -y
+conda activate storm
 conda list
 conda activate base
